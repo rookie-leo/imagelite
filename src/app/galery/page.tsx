06ad1d432c02a9b029591ae1783556a777fc3af5
rookie-leo <proleonardopa@gmail.com>
2026-01-04 16:56:13 -0300

    ImageCards component implemented

--- src/app/galery/page.tsx
+++ src/app/galery/page.tsx
@@ -1,9 +1,13 @@
-import { Template } from "@/components"
+import { ImageCards, Template } from "@/components"
 
 export default function GaleryPage() {
     return (
         <Template>
-            <h1>Galeria</h1>
+            <section className="grid grid-cols-3 gap-8">
+                <ImageCards name="Naruto Uzumaki Fases" src="https://upload.wikimedia.org/wikipedia/pt/e/e3/NarutoUzumakiTresFases.png" dataUpload="04/01/2024"/>
+                <ImageCards />
+                <ImageCards />
+            </section>
         </Template>
     )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { Template } from './Template'
+export { ImageCards } from './ImageCards'
