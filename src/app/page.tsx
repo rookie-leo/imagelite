3e9e04f85488c10b278f9ae98efd830e4dced96b
rookie-leo <proleonardopa@gmail.com>
2026-01-04 14:21:29 -0300

    Barrels Pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { TestComponent,ArrowFunction } from "../components/TestComponent"
+import { TestComponent,ArrowFunction } from "@/components/TestComponent"
 
 export default function Home() {
   return (
