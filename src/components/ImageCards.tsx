2fe8b03c150d5403a937eaeeceeafc016b9758cb
rookie-leo <proleonardopa@gmail.com>
2026-01-07 22:28:33 -0300

    Download button and image expand implemented

--- src/components/ImageCards.tsx
+++ src/components/ImageCards.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface ImageCardsProps {
     name?: string,
     tamanho?: string,
@@ -8,14 +10,40 @@ interface ImageCardsProps {
 export const ImageCards: React.FC<ImageCardsProps> = ({
     name, dataUpload, tamanho, src
 }: ImageCardsProps) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function download() {
+        window.open(src, '_blank')
+    }
+
     return (
-        <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:translate-y-2">
-            <img src={ src } className="h-56 w-full object-cover rounded-t-md" alt="" />
-            <div className="card-body p-4">
-                <h5 className="text-xl font-semibold mb-2 text-gray-600">{ name }</h5>
-                <p className="text-gray-600">{ tamanho }</p>
-                <p className="text-gray-600">{ dataUpload }</p>
+        <>
+            <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:translate-y-2">
+                <img onClick={() => setIsOpen(true)} src={src} className="h-56 w-full object-cover rounded-t-md " alt="" />
+                <div className="card-body p-4">
+                    <h5 className="text-xl font-semibold mb-2 text-gray-600">{name}</h5>
+                    <p className="text-gray-600">{tamanho}</p>
+                    <p className="text-gray-600">{dataUpload}</p>
+                    <button onClick={download} className="bg-blue-500 hover:bg-blue-900 text-white px-4 py-2 rounded-lg transition-colors duration-300">Download</button>
+                </div>
             </div>
-        </div>
+
+            {isOpen && (
+                <div
+                    className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 "
+                    onClick={() => setIsOpen(false)}
+                >
+                    <img
+                        src={src}
+                        alt={name}
+                        className="max-h-[90vh] max-w-[90vw] rounded-lg shadow-lg "
+                        onClick={(e) => e.stopPropagation}
+                    />
+
+                </div>
+            )
+
+            }
+        </>
     )
 }
