195917a37275eb4b860d9e0e66ac93e58bce58cc
rookie-leo <proleonardopa@gmail.com>
2026-01-11 21:46:46 -0300

    Form validation implemented

--- src/app/forms/formeScheme.ts
+++ src/app/forms/formeScheme.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 export interface FormsProps {
     name: string,
     tags: string,
-    file: any
+    file: string | Blob
 }
 
 export const formScheme: FormsProps = { name: '', tags: '', file: '' }
@@ -17,5 +17,14 @@ export const formValidation = Yup.object().shape({
     tags: Yup
             .string()
             .trim()
-            .required("O campo tags é obrigatório!")
+            .required("O campo tags é obrigatório!"),
+    file: Yup
+            .mixed<Blob>()
+            .required("É necessario realizar o uploado do arquivo antes de salvar!")
+            .test('size', 'O arquivo não pode ser maior que 50 MB', (file) => {
+                return file.size < 50000
+            })
+            .test('type', 'Formatos aceitos: JPEG, GIFF ou PNG', (file) => {
+                return file.type === 'image/jpeg' || file.type === 'image/gif' || file.type === 'image/png'
+            })
 })
